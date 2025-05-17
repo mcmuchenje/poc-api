3df0ac80b8f9f53fc9f3db411e3c633e95f61eae
munyac <muchenje.munyaradzi@gmail.com>
2025-05-17 19:09:00 +0200

    clean

--- controllers/radcheck.controller.js
+++ controllers/radcheck.controller.js
@@ -30,6 +30,7 @@ exports.getOne = async (req, res) => {
 
 exports.getMany = async (req, res) => {
     try {
+        res.setHeader('X-Total-Count', 319)
         const users = await db.radcheck.findAll();
 
         res.send(users);
