8208c157f70151065103e9d52f6e11fb5ff56771
munyac <muchenje.munyaradzi@gmail.com>
2025-05-17 19:45:47 +0200

    next

--- controllers/radcheck.controller.js
+++ controllers/radcheck.controller.js
@@ -31,23 +31,23 @@ exports.getOne = async (req, res) => {
 exports.getMany = async (req, res) => {
     console.log(req.query);
     try {
-        res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count');
-        res.setHeader('X-Total-Count', 319);
-
         const start = Number(req.query._start);
         const end = Number(req.query._end);
 
         const limit = end - start + 1;
         const offset = start;
 
-        const users = await db.radcheck.findAll({
-            // limit,
-            // offset,
+        const users = await db.radcheck.findAndCountAll({
+            limit,
+            offset,
             order: [[req.query._sort, req.query._order]],
         });
 
-        res.send(users);
+        res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count');
+        res.setHeader('X-Total-Count', users.count);
 
+        res.send(users.rows);
+        
     } catch (err) {
         console.log(err.message);
     }
